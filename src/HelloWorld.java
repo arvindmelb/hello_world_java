4e0fecbe6e88b3e1cbfd02245325dbc76043b4cd
Arvind Kumar <arvind.melb@gmail.com>
2012-06-15 17:14:28 +1000

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 6/14/12
+ * Time: 12:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] argv) throws IOException {
+        BufferedReader buff = new BufferedReader(
+                new InputStreamReader(System.in));
+        System.out.print("What's your name? ");
+        System.out.flush();
+        String s = buff.readLine();
+        System.out.printf("Hello, %s!", s);
+    }
+}
---
+++ tests/HelloWorldTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+* Created with IntelliJ IDEA.
+* User: Arvind Kumar
+* Date: 6/15/12
+* Time: 5:00 PM
+* To change this template use File | Settings | File Templates.
+*/
+public class HelloWorldTest {
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
